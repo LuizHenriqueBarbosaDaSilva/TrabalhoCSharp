d3e1db5fd32b85229d003e6b0aab2a7c41750082
LuizHenriqueBarbosaDaSilva <luizbarbosa@gmail.com>
2024-09-01 18:00:58 -0300

    Feito a ordenação!

--- TesteTrabalho/TesteDosMetodos.cs
+++ TesteTrabalho/TesteDosMetodos.cs
@@ -7,8 +7,17 @@ namespace TesteTrabalho
         [TestMethod]
         public void TestandoFatorialDe5()
         {
-            // Cenario  Ação & Teste
-            Assert.AreEqual(120,Fatorial.CalcularFatorial(5), "O fatorial de 5 deveria dar 120");
+            // Cenario, Ação & Teste
+            Assert.AreEqual(120,Fatorial.CalcularFatorial(5), "O fatorial de 5 deveria dar 120!");
+        }
+        [TestMethod]
+        public void TestandoOrdenacao()
+        {
+            // Cenario  
+            int[] resultado = { 1, 4, 5, 6, 7, 8 };
+            int[] vetorFora = { 8, 6, 5, 7, 4, 1 };
+            // Ação & Teste
+            CollectionAssert.AreEqual(resultado, Ordenador.Ordenacao(vetorFora), "A lista não foi ordenada corretamente!");
         }
     }
 }
--- Trabalho1CSHarp/UteisMenu/Ordenador.cs
+++ Trabalho1CSHarp/UteisMenu/Ordenador.cs
@@ -6,7 +6,26 @@ using System.Threading.Tasks;
 
 namespace Trabalho1CSHarp.UteisMenu
 {
-    internal class Ordenador
+    public class Ordenador
     {
+        public static int[] Ordenacao(int[] _vetor)
+        {
+            int _tamanhoLista = _vetor.Length;
+            for (int i = 1; i < _tamanhoLista; i++)
+            {
+                int key = _vetor[i];
+                int j = i - 1;
+
+                // Move os elementos do vetor que são maiores que o key
+                // para uma posição à frente da sua posição atual
+                while (j >= 0 && _vetor[j] > key)
+                {
+                    _vetor[j + 1] = _vetor[j];
+                    j = j - 1;
+                }
+                _vetor[j + 1] = key;
+            }
+            return _vetor;
+        }
     }
 }
